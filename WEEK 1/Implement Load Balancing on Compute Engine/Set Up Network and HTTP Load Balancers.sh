d9952f8e333cca4f18cbcba64235ff6e8228982f
Destin Kabote Lupaya <84081391+Destin-Lupaya@users.noreply.github.com>
2024-04-05 00:45:57 +0200

    challenge lab

--- WEEK 1/Implement Load Balancing on Compute Engine/Set Up Network and HTTP Load Balancers.sh
+++ WEEK 1/Implement Load Balancing on Compute Engine/Set Up Network and HTTP Load Balancers.sh
@@ -388,3 +388,29 @@
 
 # Copyright 2024 Google LLC All rights reserved. Google and the Google logo are trademarks of Google LLC. All other company and product names may be trademarks of the respective companies with which they are associated.
 
+# Implement Load Balancing on Compute Engine: Challenge Lab
+# Export all the values carefully
+# export INSTANCE_NAME=
+
+# export ZONE=
+
+# export PORT=
+
+# export FIREWALL_NAME=
+# NOW JUST COPY THE CODE AND PASTE ON YOUR CLOUD SHELL
+# curl -LO raw.githubusercontent.com/quiccklabs/Labs_solutions/master/Implement%20Load%20Balancing%20on%20Compute%20Engine%20Challenge%20Lab/quicklabgsp313.sh
+# sudo chmod +x quicklabgsp313.sh
+# ./quicklabgsp313.sh
+# Lab might task 10 - 15 mintues to updated the score so don't worry!
+# Screenshot-2024-03-25-at-7-47-33-PM.png
+
+# Congratulations !!!
+
+# export INSTANCE_NAME=nucleous-jumphost-129
+# export ZONE= europe-west4-a
+# export PORT=8082
+# export FIREWALL_NAME=allow-tcp-rule-594
+
+# curl -LO raw.githubusercontent.com/quiccklabs/Labs_solutions/master/Implement%20Load%20Balancing%20on%20Compute%20Engine%20Challenge%20Lab/quicklabgsp313.sh
+# sudo chmod +x quicklabgsp313.sh
+# ./quicklabgsp313.sh
