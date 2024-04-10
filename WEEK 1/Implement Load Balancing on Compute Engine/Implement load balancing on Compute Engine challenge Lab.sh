11ec4b6042dc5197e8a8f827d1da0a4607337098
Destin Kabote Lupaya <84081391+Destin-Lupaya@users.noreply.github.com>
2024-04-10 11:41:13 +0200

    Resolution challenge

--- WEEK 1/Implement Load Balancing on Compute Engine/Implement load balancing on Compute Engine challenge Lab.sh
+++ WEEK 1/Implement Load Balancing on Compute Engine/Implement load balancing on Compute Engine challenge Lab.sh
@@ -145,3 +145,43 @@
 
 # Ready for more?
 # Here's another lab we think you'll like.
+
+RESOLUTION
+
+https://github.com/quiccklabs/Labs_solutions/blob/abfb97e25e85694d40b10bc608c148a295eea078/Implement%20Load%20Balancing%20on%20Compute%20Engine%20Challenge%20Lab/quicklab.md
+
+
+
+
+## ***```Implement Load Balancing on Compute Engine: Challenge Lab```***
+
+### Export all the values carefully
+
+```bash
+export INSTANCE_NAME=
+
+export ZONE=
+
+export PORT=
+
+export FIREWALL_NAME=
+```
+###
+###
+
+### ***NOW JUST COPY THE CODE AND PASTE ON YOUR CLOUD SHELL***
+###
+###
+
+```bash 
+curl -LO raw.githubusercontent.com/quiccklabs/Labs_solutions/master/Implement%20Load%20Balancing%20on%20Compute%20Engine%20Challenge%20Lab/quicklabgsp313.sh
+sudo chmod +x quicklabgsp313.sh
+./quicklabgsp313.sh
+```
+
+### Lab might task 10 - 15 mintues to updated the score so don't worry!
+
+# [![Screenshot-2024-03-25-at-7-47-33-PM.png](https://i.postimg.cc/Vk2hdZfK/Screenshot-2024-03-25-at-7-47-33-PM.png)](https://postimg.cc/zyS7QjRh)
+
+
+### Congratulations !!!
--- WEEK 3/Build a Secure Google Cloud Network/Build a Secure Google Cloud Network Challenge Lab.sh
+++ WEEK 3/Build a Secure Google Cloud Network/Build a Secure Google Cloud Network Challenge Lab.sh
@@ -81,3 +81,43 @@
 # Manual Last Tested November 08, 2023
 
 # Copyright 2024 Google LLC All rights reserved. Google and the Google logo are trademarks of Google LLC. All other company and product names may be trademarks of the respective companies with which they are associated.
+
+# RESOLUTION
+
+# https://www.youtube.com/watch?v=GK3oBOgORbI
+
+# Build and Secure Networks in Google Cloud: Challenge Lab
+
+
+export IAP_NETWORK_TAG=allow-ssh-iap-ingress-ql-800
+
+export INTERNAL_NETWORK_TAG=allow-ssh-internal-ingress-ql-800
+
+export HTTP_NETWORK_TAG=allow-http-ingress-ql-800
+
+export ZONE=us-central1-a
+
+
+gcloud compute firewall-rules delete open-access
+ 
+
+gcloud compute firewall-rules create ssh-ingress --allow=tcp:22 --source-ranges 35.235.240.0/20 --target-tags $IAP_NETWORK_TAG --network acme-vpc
+ 
+gcloud compute instances add-tags bastion --tags=$IAP_NETWORK_TAG --zone=$ZONE
+ 
+
+gcloud compute firewall-rules create http-ingress --allow=tcp:80 --source-ranges 0.0.0.0/0 --target-tags $HTTP_NETWORK_TAG --network acme-vpc
+ 
+gcloud compute instances add-tags juice-shop --tags=$HTTP_NETWORK_TAG --zone=$ZONE
+ 
+
+gcloud compute firewall-rules create internal-ssh-ingress --allow=tcp:22 --source-ranges 192.168.10.0/24 --target-tags $INTERNAL_NETWORK_TAG --network acme-vpc
+ 
+gcloud compute instances add-tags juice-shop --tags=$INTERNAL_NETWORK_TAG --zone=$ZONE
+ 
+
+
+# Task 6 : SSH to bastion host via IAP and juice-shop via bastion
+# In Compute Engine -> VM Instances page, click the SSH button for the bastion host. Then SSH to juice-shop by
+ 
+gcloud compute ssh juice-shop --internal-ip
