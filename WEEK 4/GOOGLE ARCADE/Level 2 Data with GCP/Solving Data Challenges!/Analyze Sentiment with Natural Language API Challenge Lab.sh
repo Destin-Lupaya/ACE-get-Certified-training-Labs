c49b64fff4dbff64b3877788afa4f563da77d1a1
Destin Kabote Lupaya <84081391+Destin-Lupaya@users.noreply.github.com>
2024-04-16 14:50:30 +0200

    Create Analyze Sentiment With Natural  language

---
+++ WEEK 4/GOOGLE ARCADE/Level 2 Data with GCP/Solving Data Challenges!/Analyze Sentiment with Natural Language API Challenge Lab.sh
@@ -0,0 +1,257 @@
+# Analyze Sentiment with Natural Language API: Challenge Lab
+# experiment
+# Lab
+# schedule
+# 45 minutes
+# universal_currency_alt
+# No cost
+# show_chart
+# Introductory
+# Lab instructions and tasks
+# ARC130
+# Overview
+# Setup and requirements
+# Challenge scenario
+# Task 1. Create an API key
+# Task 2. Set up Google Docs and call the Natural Language API
+# Task 3. Analyze syntax and parts of speech with the Natural Language API
+# Task 4. Perform multilingual natural language processing
+# Congratulations!
+# ARC130
+# Google Cloud self-paced labs logo
+
+# Overview
+# In a challenge lab you’re given a scenario and a set of tasks. Instead of following step-by-step instructions, you will use the skills learned from the labs in the course to figure out how to complete the tasks on your own! An automated scoring system (shown on this page) will provide feedback on whether you have completed your tasks correctly.
+
+# When you take a challenge lab, you will not be taught new Google Cloud concepts. You are expected to extend your learned skills, like changing default values and reading and researching error messages to fix your own mistakes.
+
+# To score 100% you must successfully complete all tasks within the time period!
+
+# Setup and requirements
+# Before you click the Start Lab button
+# Read these instructions. Labs are timed and you cannot pause them. The timer, which starts when you click Start Lab, shows how long Google Cloud resources will be made available to you.
+
+# This hands-on lab lets you do the lab activities yourself in a real cloud environment, not in a simulation or demo environment. It does so by giving you new, temporary credentials that you use to sign in and access Google Cloud for the duration of the lab.
+
+# To complete this lab, you need:
+
+# Access to a standard internet browser (Chrome browser recommended).
+# Note: Use an Incognito or private browser window to run this lab. This prevents any conflicts between your personal account and the Student account, which may cause extra charges incurred to your personal account.
+# Time to complete the lab---remember, once you start, you cannot pause a lab.
+# Note: If you already have your own personal Google Cloud account or project, do not use it for this lab to avoid extra charges to your account.
+# Challenge scenario
+# You recently joined an organization and are working as a junior cloud engineer as part of a team. You have been assigned machine learning (ML) projects and one of your client requirements is to use the Cloud Natural Language API service in Google Cloud to perform tasks for the completion of a project.
+
+# You are expected to have the skills and knowledge for the tasks that follow.
+
+# Your challenge
+# For this challenge, you are asked to set up Google Docs and perform sentiment analysis on some reviews provided by customers, analyze syntax and parts of speech using the Natural language API, and create a Natural Language API request for a language other than English.
+
+# You need to:
+
+# Create an API key.
+# Set up Google Docs and call the Natural Language API.
+# Analyze syntax and parts of speech with the Natural Language API.
+# Perform multilingual natural language processing.
+# For this challenge lab, a virtual machine (VM) instance named Instance name has been configured for you to complete tasks 3 and 4.
+
+# Some standards you should follow:
+
+# Ensure that any needed APIs (such as the Cloud Natural Language API) are successfully enabled.
+# Each task is described in detail below, good luck!
+
+# Task 1. Create an API key
+# For this task, you need to create an API key to use in this and other tasks when sending a request to the Natural Language API.
+
+# Save the API key to use in other tasks.
+
+# Click Check my progress to verify the objective.
+# Create an API key
+
+# Task 2. Set up Google Docs and call the Natural Language API
+# For this task, you need to set up Google Docs to use the Natural Language API and recognize the sentiment of selected text in a Google Docs document and highlight parts of it based on sentiment.
+
+# Text is highlighted in red for negative sentiment, green for positive sentiment, and yellow for neutral sentiment.
+
+# Create a new Google Docs document.
+
+# Use the following code in Apps Script. Modify the code to add your API key and enter the URL of the Natural Language API to analyze entity sentiment.
+
+#   /**
+#   * @OnlyCurrentDoc
+#   *
+#   * The above comment directs Apps Script to limit the scope of file
+#   * access for this add-on. It specifies that this add-on will only
+#   * attempt to read or modify the files in which the add-on is used,
+#   * and not all of the user's files. The authorization request message
+#   * presented to users will reflect this limited scope.
+#   */
+
+#   /**
+#   * Creates a menu entry in the Google Docs UI when the document is
+#   * opened.
+#   *
+#   */
+#   function onOpen() {
+#     var ui = DocumentApp.getUi();
+#     ui.createMenu('Natural Language Tools')
+#       .addItem('Mark Sentiment', 'markSentiment')
+#       .addToUi();
+#   }
+#   /**
+#   * Gets the user-selected text and highlights it based on sentiment
+#   * with green for positive sentiment, red for negative, and yellow
+#   * for neutral.
+#   *
+#   */
+#   function markSentiment() {
+#     var POSITIVE_COLOR = '#00ff00';  //  Colors for sentiments
+#     var NEGATIVE_COLOR = '#ff0000';
+#     var NEUTRAL_COLOR = '#ffff00';
+#     var NEGATIVE_CUTOFF = -0.2;   //  Thresholds for sentiments
+#     var POSITIVE_CUTOFF = 0.2;
+
+#     var selection = DocumentApp.getActiveDocument().getSelection();
+#     if (selection) {
+#       var string = getSelectedText();
+
+#       var sentiment = retrieveSentiment(string);
+
+#       //  Select the appropriate color
+#       var color = NEUTRAL_COLOR;
+#       if (sentiment <= NEGATIVE_CUTOFF) {
+#         color = NEGATIVE_COLOR;
+#       }
+#       if (sentiment >= POSITIVE_CUTOFF) {
+#         color = POSITIVE_COLOR;
+#       }
+
+#       //  Highlight the text
+#       var elements = selection.getSelectedElements();
+#       for (var i = 0; i < elements.length; i++) {
+#         if (elements[i].isPartial()) {
+#           var element = elements[i].getElement().editAsText();
+#           var startIndex = elements[i].getStartOffset();
+#           var endIndex = elements[i].getEndOffsetInclusive();
+#           element.setBackgroundColor(startIndex, endIndex, color);
+
+#         } else {
+#           var element = elements[i].getElement().editAsText();
+#           foundText = elements[i].getElement().editAsText();
+#           foundText.setBackgroundColor(color);
+#         }
+#       }
+#     }
+#   }
+#   /**
+#   * Returns a string with the contents of the selected text.
+#   * If no text is selected, returns an empty string.
+#   */
+#   function getSelectedText() {
+#     var selection = DocumentApp.getActiveDocument().getSelection();
+#     var string = "";
+#     if (selection) {
+#       var elements = selection.getSelectedElements();
+
+#       for (var i = 0; i < elements.length; i++) {
+#         if (elements[i].isPartial()) {
+#           var element = elements[i].getElement().asText();
+#           var startIndex = elements[i].getStartOffset();
+#           var endIndex = elements[i].getEndOffsetInclusive() + 1;
+#           var text = element.getText().substring(startIndex, endIndex);
+#           string = string + text;
+
+#         } else {
+#           var element = elements[i].getElement();
+#           // Only translate elements that can be edited as text; skip
+#           // images and other non-text elements.
+#           if (element.editAsText) {
+#             string = string + element.asText().getText();
+#           }
+#         }
+#       }
+#     }
+#     return string;
+#   }
+
+#   /** Given a string, will call the Natural Language API and retrieve
+#     * the sentiment of the string.  The sentiment will be a real
+#     * number in the range -1 to 1, where -1 is highly negative
+#     * sentiment and 1 is highly positive.
+#   */
+#   function retrieveSentiment (line) {
+#   var apiKey = "your key here";
+#   var apiEndpoint =
+# 'Enter the URL of Natural Language API to Analyze Entity Sentiment'
+# + apiKey;
+#   //  Create a structure with the text, its language, its type,
+#   //  and its encoding
+#   var docDetails = {
+#     language: 'en-us',
+#     type: 'PLAIN_TEXT',
+#     content: line
+#   };
+#   var nlData = {
+#     document: docDetails,
+#     encodingType: 'UTF8'
+#   };
+#   //  Package all of the options and the data together for the call
+#   var nlOptions = {
+#     method : 'post',
+#     contentType: 'application/json',
+#     payload : JSON.stringify(nlData)
+#   };
+#   //  And make the call
+#   var response = UrlFetchApp.fetch(apiEndpoint, nlOptions);
+#   var data = JSON.parse(response);
+#   var sentiment = 0.0;
+#   //  Ensure all pieces were in the returned value
+#   if (data && data.documentSentiment
+#           && data.documentSentiment.score){
+#      sentiment = data.documentSentiment.score;
+#   }
+#   return sentiment;
+# }
+# Copied!
+# Add text to your document. You can use the sample that comes from Charles Dickens' novel, A Tale of Two Cities.
+# Click Check my progress to verify the objective.
+# Set up Google Docs and call the Natural Language API
+
+# Task 3. Analyze syntax and parts of speech with the Natural Language API
+# To complete this task, connect via SSH to the VM instance named Instance name that has been provisioned for you.
+
+# Create a JSON file called analyze-request.json using the code that follows.
+# {
+#   "document":{
+#     "type":"PLAIN_TEXT",
+#     "content": "Google, headquartered in Mountain View, unveiled the new Android phone at the Consumer Electronic Show.  Sundar Pichai said in his keynote that users love their new Android phones."
+#   },
+#   "encodingType": "UTF8"
+# }
+# Copied!
+# Pass your request (along with the API key environment variable you saved earlier in task 1) to the Natural Language API using the curl command or analyze syntax using gcloud ML commands.
+
+# Save the response in a file called analyze-response.txt.
+
+# Click Check my progress to verify the objective.
+# Analyze syntax and parts of speech with the Natural Language API
+
+# Task 4. Perform multilingual natural language processing
+# To complete this task, connect via SSH to the VM instance named Instance name that has been provisioned for you.
+
+# Create a JSON file called multi-nl-request.json using the code that follows, which contains a sentence in the French language.
+# {
+#   "document":{
+#     "type":"PLAIN_TEXT",
+#     "content":"Le bureau japonais de Google est situé à Roppongi Hills, Tokyo."
+#   }
+# }
+# Copied!
+# Pass your request (along with the API key environment variable you saved earlier in task 1) to the Natural Language API using the curl command or analyze syntax using gcloud ML commands.
+
+# Save the output in a file called multi-response.txt.
+
+# Click Check my progress to verify the objective.
+# Perform multilingual natural language processing
+
+# Congratulations!
---
+++ WEEK 4/GOOGLE ARCADE/Level 2 Data with GCP/Solving Data Challenges!/Analyze Speech & Language with Google APIs Challenge Lab.sh
@@ -0,0 +1,105 @@
+# Google Cloud self-paced labs logo
+# Overview
+# In a challenge lab you’re given a scenario and a set of tasks. Instead of following step-by-step instructions, you will use the skills learned from the labs in the course to figure out how to complete the tasks on your own! An automated scoring system (shown on this page) will provide feedback on whether you have completed your tasks correctly.
+
+# When you take a challenge lab, you will not be taught new Google Cloud concepts. You are expected to extend your learned skills, like changing default values and reading and researching error messages to fix your own mistakes.
+
+# To score 100% you must successfully complete all tasks within the time period!
+
+# Setup and requirements
+# Before you click the Start Lab button
+# Read these instructions. Labs are timed and you cannot pause them. The timer, which starts when you click Start Lab, shows how long Google Cloud resources will be made available to you.
+
+# This hands-on lab lets you do the lab activities yourself in a real cloud environment, not in a simulation or demo environment. It does so by giving you new, temporary credentials that you use to sign in and access Google Cloud for the duration of the lab.
+
+# To complete this lab, you need:
+
+# Access to a standard internet browser (Chrome browser recommended).
+# Note: Use an Incognito or private browser window to run this lab. This prevents any conflicts between your personal account and the Student account, which may cause extra charges incurred to your personal account.
+# Time to complete the lab---remember, once you start, you cannot pause a lab.
+# Note: If you already have your own personal Google Cloud account or project, do not use it for this lab to avoid extra charges to your account.
+# Challenge scenario
+# You are starting your career as a junior cloud architect. In this role, you have been assigned to work on a team project that requires you to use the Cloud Natural Language API and Cloud Speech API services in Google Cloud.
+
+# You are expected to have the skills and knowledge to complete the tasks that follow.
+
+# Your challenge
+# For this challenge, you are asked to analyze some text and speech using the Cloud Natural Language API and Cloud Speech APIs, respectively. You also need to perform sentiment analysis on a text document using Python.
+
+# You need to:
+
+# Create an API key
+# Make an entity analysis request and call the Natural Language API
+# Create a speech analysis request and call the Speech API
+# Analyze sentiment with the Natural Language API
+# For this challenge lab, a virtual machine (VM) instance named Instance name has been configured for you to complete the tasks.
+
+# Each task is described in detail below, good luck!
+
+# Task 1. Create an API key
+# For this task, you need to create an API key to use in this and other tasks when sending a request to the Natural Language API.
+
+# Save the API key to use in other tasks.
+
+# Click Check my progress to verify the objective.
+# Create an API key
+
+# Task 2. Make an entity analysis request and call the Natural Language API
+# For this task, connect to the instance Instance name provisioned for you via SSH.
+
+# Next, create a JSON file named nl_request.json which you will pass to the Natural Language API for analysis. You can add the following code to your JSON file to analyze text about the city of Boston or, alternatively, add text of your own choosing to the content object to perform entity analysis on that instead.
+
+# {
+#   "document":{
+#     "type":"PLAIN_TEXT",
+#     "content":"With approximately 8.2 million people residing in Boston, the capital city of Massachusetts is one of the largest in the United States."
+#   },
+#   "encodingType":"UTF8"
+# }
+# Copied!
+# You can now pass your request body, along with the API key environment variable you saved earlier, to the Natural Language API using the curl command or analyze the text using gcloud ML commands.
+
+# Save the response in a file called nl_response.json.
+
+# Click Check my progress to verify the objective.
+# Make an entity analysis request and call the Natural Language API
+
+# Task 3. Create a speech analysis request and call the Speech API
+# Note: For this task, you will use a pre-recorded file that's available on Cloud Storage: gs://cloud-samples-tests/speech/brooklyn.flac. Listen to the audio file before sending it to the Speech API.
+# Create another JSON file, named speech_request.json for this task, and add the content using the URI value of the sample audio file.
+# {
+#   "config": {
+#       "encoding":"FLAC",
+#       "languageCode": "en-US"
+#   },
+#   "audio": {
+#       "uri":"Pass the API the uri of the audio file in Cloud Storage"
+#   }
+# }
+# Copied!
+# You can now pass your request body, along with the API key environment variable that you saved earlier, to the Natural Language API using the curl command or analyze the speech using gcloud ML commands.
+
+# Save the response in a file named speech_response.json.
+
+# Click Check my progress to verify the objective.
+# Create a speech analysis request and call the Speech API
+
+# Task 4. Analyze sentiment with the Natural Language API
+# For this task, you need to analyze text sentiment using the Google Cloud Natural Language API, which attempts to determine the overall attitude (positive or negative) of a content sample such as a movie review. In the Instance name instance, a simple Python application code file called sentiment_analysis.py has already been configured and created for you. To perform your analysis, you'll test it on a set of (fictitious or fake) movie reviews for the 1982 sci-fi action film, Blade Runner, directed by Ridley Scott.
+
+# To use the Natural Language API to perform sentiment analysis, you need to access the service by calling the analyze_sentiment method of the LanguageServiceClient instance.
+
+# You need to edit the method def analyze(movie_review_filename): in the file sentiment_analysis.py and complete the method using Python code that performs the following actions:
+
+# Instantiate a LanguageServiceClient instance as the client.
+# Read the filename containing the text data into a variable.
+# Instantiate a Document object with the contents of the file.
+# Call the client's analyze_sentiment method.
+# Download the fictitious movie review samples from Google Cloud Storage: gs://cloud-samples-tests/natural-language/sentiment-samples.tgz .
+
+# Unzip the sample files and run the sentiment analysis on one of the files, bladerunner-pos.txt, using the relevant Python command.
+
+# Click Check my progress to verify the objective.
+# Analyze sentiment with the Natural Language API
+
+# Congratulations!
