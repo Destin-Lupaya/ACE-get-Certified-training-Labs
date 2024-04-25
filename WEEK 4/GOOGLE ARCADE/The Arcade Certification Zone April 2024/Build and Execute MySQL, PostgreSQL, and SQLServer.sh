3a8225e43b22a0ac45cc8efe84ff37663a4d6769
Destin Kabote Lupaya <84081391+Destin-Lupaya@users.noreply.github.com>
2024-04-25 13:09:22 +0200

    UPDATE

--- WEEK 4/GOOGLE ARCADE/The Arcade Certification Zone April 2024/Build and Execute MySQL, PostgreSQL, and SQLServer.sh
+++ WEEK 4/GOOGLE ARCADE/The Arcade Certification Zone April 2024/Build and Execute MySQL, PostgreSQL, and SQLServer.sh
@@ -533,3 +533,181 @@
 
 # Copyright 2024 Google LLC All rights reserved. Google and the Google logo are trademarks of Google LLC. All other company and product names may be trademarks of the respective companies with which they are associated.
 
+# resolution
+export REGION=us-central1
+
+curl -LO raw.githubusercontent.com/QUICK-GCP-LAB/2-Minutes-Labs-Solutions/main/Build%20and%20Execute%20MySQL%20PostgreSQL%20and%20SQLServer%20to%20Data%20Catalog%20Connectors/gsp814.sh
+
+sudo chmod +x gsp814.sh
+
+./gsp814.sh
+
+
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${YELLOW}${BOLD}Starting${RESET}" "${GREEN}${BOLD}Execution${RESET}"
+
+gcloud services enable datacatalog.googleapis.com
+
+echo "${YELLOW}${BOLD}Task 1. ${RESET}""${WHITE}${BOLD}Enable the Data Catalog API${RESET}" "${GREEN}${BOLD}Completed${RESET}"
+
+export PROJECT_ID=$(gcloud config get-value project)
+
+gsutil cp gs://spls/gsp814/cloudsql-sqlserver-tooling.zip .
+unzip cloudsql-sqlserver-tooling.zip
+
+cd cloudsql-sqlserver-tooling/infrastructure/terraform
+
+sed -i "s/us-central1/$REGION/g" variables.tf
+
+cd ~/cloudsql-sqlserver-tooling
+bash init-db.sh
+
+gcloud iam service-accounts create sqlserver2dc-credentials \
+--display-name  "Service Account for SQL Server to Data Catalog connector" \
+--project $PROJECT_ID
+
+gcloud iam service-accounts keys create "sqlserver2dc-credentials.json" \
+--iam-account "sqlserver2dc-credentials@$PROJECT_ID.iam.gserviceaccount.com"
+
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+--member "serviceAccount:sqlserver2dc-credentials@$PROJECT_ID.iam.gserviceaccount.com" \
+--quiet \
+--project $PROJECT_ID \
+--role "roles/datacatalog.admin"
+
+cd infrastructure/terraform/
+
+public_ip_address=$(terraform output -raw public_ip_address)
+username=$(terraform output -raw username)
+password=$(terraform output -raw password)
+database=$(terraform output -raw db_name)
+
+cd ~/cloudsql-sqlserver-tooling
+
+docker run --rm --tty -v \
+"$PWD":/data mesmacosta/sqlserver2datacatalog:stable \
+--datacatalog-project-id=$PROJECT_ID \
+--datacatalog-location-id=$REGION \
+--sqlserver-host=$public_ip_address \
+--sqlserver-user=$username \
+--sqlserver-pass=$password \
+--sqlserver-database=$database
+
+echo "${YELLOW}${BOLD}Task 2. ${RESET}""${WHITE}${BOLD}SQL Server to Dataplex${RESET}" "${GREEN}${BOLD}Completed${RESET}"
+
+cd
+
+gsutil cp gs://spls/gsp814/cloudsql-postgresql-tooling.zip .
+unzip cloudsql-postgresql-tooling.zip
+
+cd cloudsql-postgresql-tooling/infrastructure/terraform
+
+sed -i "s/us-central1/$REGION/g" variables.tf
+
+cd ~/cloudsql-postgresql-tooling
+bash init-db.sh
+
+gcloud iam service-accounts create postgresql2dc-credentials \
+--display-name  "Service Account for PostgreSQL to Data Catalog connector" \
+--project $PROJECT_ID
+
+gcloud iam service-accounts keys create "postgresql2dc-credentials.json" \
+--iam-account "postgresql2dc-credentials@$PROJECT_ID.iam.gserviceaccount.com"
+
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+--member "serviceAccount:postgresql2dc-credentials@$PROJECT_ID.iam.gserviceaccount.com" \
+--quiet \
+--project $PROJECT_ID \
+--role "roles/datacatalog.admin"
+
+cd infrastructure/terraform/
+
+public_ip_address=$(terraform output -raw public_ip_address)
+username=$(terraform output -raw username)
+password=$(terraform output -raw password)
+database=$(terraform output -raw db_name)
+
+cd ~/cloudsql-postgresql-tooling
+
+docker run --rm --tty -v \
+"$PWD":/data mesmacosta/postgresql2datacatalog:stable \
+--datacatalog-project-id=$PROJECT_ID \
+--datacatalog-location-id=$REGION \
+--postgresql-host=$public_ip_address \
+--postgresql-user=$username \
+--postgresql-pass=$password \
+--postgresql-database=$database
+
+
+echo "${YELLOW}${BOLD}Task 3. ${RESET}""${WHITE}${BOLD}PostgreSQL to Dataplex${RESET}" "${GREEN}${BOLD}Completed${RESET}"
+
+cd
+
+gsutil cp gs://spls/gsp814/cloudsql-mysql-tooling.zip .
+unzip cloudsql-mysql-tooling.zip
+
+cd cloudsql-mysql-tooling/infrastructure/terraform
+
+sed -i "s/us-central1/$REGION/g" variables.tf
+
+cd ~/cloudsql-mysql-tooling
+bash init-db.sh
+
+gcloud iam service-accounts create mysql2dc-credentials \
+--display-name  "Service Account for MySQL to Data Catalog connector" \
+--project $PROJECT_ID
+
+gcloud iam service-accounts keys create "mysql2dc-credentials.json" \
+--iam-account "mysql2dc-credentials@$PROJECT_ID.iam.gserviceaccount.com"
+
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+--member "serviceAccount:mysql2dc-credentials@$PROJECT_ID.iam.gserviceaccount.com" \
+--quiet \
+--project $PROJECT_ID \
+--role "roles/datacatalog.admin"
+
+cd infrastructure/terraform/
+
+public_ip_address=$(terraform output -raw public_ip_address)
+username=$(terraform output -raw username)
+password=$(terraform output -raw password)
+database=$(terraform output -raw db_name)
+
+cd ~/cloudsql-mysql-tooling
+
+docker run --rm --tty -v \
+"$PWD":/data mesmacosta/mysql2datacatalog:stable \
+--datacatalog-project-id=$PROJECT_ID \
+--datacatalog-location-id=$REGION \
+--mysql-host=$public_ip_address \
+--mysql-user=$username \
+--mysql-pass=$password \
+--mysql-database=$database
+
+echo "${YELLOW}${BOLD}Task 4. ${RESET}""${WHITE}${BOLD}MySQL to Dataplex${RESET}" "${GREEN}${BOLD}Completed${RESET}"
+
+echo "${RED}${BOLD}Congratulations${RESET}" "${WHITE}${BOLD}for${RESET}" "${GREEN}${BOLD}Completing the Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
